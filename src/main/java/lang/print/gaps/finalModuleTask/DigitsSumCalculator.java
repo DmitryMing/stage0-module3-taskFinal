c0a87da48d6f69b861c01442066b324ee394d916
dmitrii <perfilev.dmitrii2020@gmail.com>
2024-05-06 19:46:40 +0800

    ff

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,16 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int res;
+        int one;
+        int second;
+        int third;
+        int four;
+        one = number % 10;
+        second = (number / 10) % 10;
+        third = (number / 100) % 10;
+        four = number / 1000;
+        res = one + second + third + four;
+        System.out.println(res);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,14 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int res;
+        int one;
+        int second;
+        int third;
+        one = number % 10;
+        second = (number / 10) % 10;
+        third = number / 100;
+        res = one * 100 + second * 10 + third;
+        System.out.println(res);
     }
 }
